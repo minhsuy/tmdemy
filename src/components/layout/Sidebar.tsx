ce1bfebd4b0e359e96a21d38da15e2bf6f48a2be
minhsuy <thaiminh0612@gmail.com>
2025-05-07 10:31:49 +0700

    asd

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -5,8 +5,16 @@ import { ActiveLink } from "../common";
 import { UserButton } from "@clerk/nextjs";
 import { IMenuItem } from "@/types/type";
 import { ModeToggle } from "../common/ModeToggle";
+import { createUser } from "@/lib/actions/user.action";
 
-const Sidebar = () => {
+const Sidebar = async () => {
+  await createUser({
+    clerkId: "clerkId",
+    name: "name",
+    username: "username",
+    email: "email",
+    avatar: "avatar",
+  });
   return (
     <div className="p-5 border-r border-r-gray-300 flex flex-col dark:bg-grayDarker dark:boder dark:border-grayDarker">
       <Link className="font-bold text-3xl inline-block mb-5" href="/">
